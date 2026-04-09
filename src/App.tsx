a11071996c4ab2892ad0a08dc6affee257a76ffa
shinnanchanguk <boogie5qnr@naver.com>
2026-04-09 16:16:07 +0900

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: 32, fontFamily: "'Helvetica Neue', Helvetica, Arial, sans-serif" }}>
+      <h1>NEIS 분할점수 계산기</h1>
+      <p>Made by DoRm</p>
+    </div>
+  );
+}
+
+export default App;
